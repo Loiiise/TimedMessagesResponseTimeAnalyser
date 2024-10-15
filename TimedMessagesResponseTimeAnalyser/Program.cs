86dbe5d06218efa4a0e609ae00e9c6b7720939fb
Loïse <izakhaak@gmail.com>
2024-10-15 18:03:55 +0200

    updated main flow

--- TimedMessagesResponseTimeAnalyser/Program.cs
+++ TimedMessagesResponseTimeAnalyser/Program.cs
@@ -1,4 +1,4 @@
-using DataTypes.ChatProfiling;
+﻿using DataTypes.ChatProfiling;
 using FileHandling;
 
 namespace TimedMessagesResponseTimeAnalyser;
@@ -7,21 +7,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string inputPath = args.Any() ? args[0] : string.Empty;
+        string inputPath = args.Any() ? args[0] : GetStringFromUser("What is the path of the file you want to analyse?");
 
-        if (inputPath == string.Empty)
-        {
-            Console.WriteLine("What is the path of the file you want to analyse?");
-            inputPath = Console.ReadLine()!;
-        }
+        var sender = GetStringFromUser("What is the name of the sender? (as used in the chat file)");
+        var receiver = GetStringFromUser("What is the name of the receiver? (as used in the chat file)");
 
         var rawMessages = FileReader.ReadAllFromPath(inputPath);
-        var profiler = new WhatsAppProfiler();
+        var profiler = new WhatsAppProfiler(sender, receiver);
 
         var chatItems = profiler.GenerateFromPlainLines(rawMessages);
 
         // todo: chat processing 
 
         // todo: output
+    }
+
+    private static string GetStringFromUser(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine()!;
     }
 }
